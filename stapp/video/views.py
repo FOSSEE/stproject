814b37250a97557c03042b76f282894aed553d40
Parth Buch <parth.buch.115@gmail.com>
2012-06-18 17:30:49 +0530

    Displaying the latest video on the home page

--- stapp/video/views.py
+++ stapp/video/views.py
@@ -3,15 +3,11 @@ from django.template import RequestContext
 from django.shortcuts import render_to_response, get_object_or_404, redirect
 from video.models import Video
 
-def show(request,video_id=None):
+def show(request):
     videos = Video.objects.all()
     if len(videos) == 0 :
         return HttpResponse("No videos in Database...")
-    play_video = None
-    if video_id != None :
-        play_video = Video.objects.get(id = video_id)
-    else:
-        play_video = videos[len(videos)-1]
+    play_video = Video.objects.latest('created')
     vids = { 'videos' : videos , 'play' : play_video}
     return render_to_response('video/list_videos.html', vids)
     
