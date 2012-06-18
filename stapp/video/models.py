bbb7ca69397b92fe958218471237b9047d82dfde
Parth Buch <parth.buch.115@gmail.com>
2012-06-18 16:45:43 +0530

    Displaying names instead of 'class' in django admin,gramatical changes

--- stapp/video/models.py
+++ stapp/video/models.py
@@ -2,14 +2,20 @@ from django.db import models
 from django.contrib.auth.models import User
 class Video(models.Model):
     """Videos to be uploaded.."""
-    video_name = models.CharField(max_length=128)
-    video_file = models.FileField(upload_to="videos")
-    video_descriotion = models.TextField()
+    name = models.CharField(max_length=128)
+    filename = models.FileField(upload_to="videos")
+    description = models.TextField()
+
+    def __unicode__(self):
+        return self.name
 
 class Module(models.Model):
     name = models.CharField(max_length=128)
     description = models.TextField()
     vidio_list = models.ManyToManyField(Video)
+
+    def __unicode__(self):
+        return self.name
 
 class Profile(models.Model):
     """User Profile describes data/information about regirstered user."""
