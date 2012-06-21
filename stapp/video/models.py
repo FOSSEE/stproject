3f342f3ac36cfbf1cb68ccb767a76aba327a8326
Parth Buch <parth.buch.115@gmail.com>
2012-06-21 18:27:21 +0530

    Added created and modified date, passing modules to the home page view

--- stapp/video/models.py
+++ stapp/video/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+import datetime
 
 class Video(models.Model):
     """Videos to be uploaded.."""
@@ -14,7 +15,14 @@ class Video(models.Model):
 class Module(models.Model):
     name = models.CharField(max_length=128)
     description = models.TextField()
-    vidio_list = models.ManyToManyField(Video)
+    video_list = models.ManyToManyField(Video)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField()
+
+    def save(self, *args, **kwargs):
+        ''' On save, update timestamps '''
+        self.modified = datetime.datetime.today()
+        super(Module, self).save(*args, **kwargs)
 
     def __unicode__(self):
         return self.name
--- stapp/video/views.py
+++ stapp/video/views.py
@@ -1,13 +1,20 @@
 from django.http import HttpResponse
 from django.template import RequestContext
-from django.shortcuts import render_to_response, get_object_or_404, redirect
-from video.models import Video
+from django.shortcuts import render, get_object_or_404, redirect
+from video.models import Video, Module
 
 def show(request):
     videos = Video.objects.all()
     if len(videos) == 0 :
         return HttpResponse("No videos in Database...")
-    play_video = Video.objects.latest('created')
-    vids = { 'videos' : videos , 'play' : play_video}
-    return render_to_response('video/list_videos.html', vids)
+
+    #Get the latest video to display on the front page
+    latest_video = Video.objects.latest('created')
+    
+    #Get last three modified modules
+    latest_modules = Module.objects.order_by('-modified')[0:3]
+
+
+    context = { 'modules' : latest_modules , 'play' : latest_video}
+    return render(request, 'video/home.html', context)
     
