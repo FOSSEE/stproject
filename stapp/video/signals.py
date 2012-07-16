e677e4659c726acc68fa75ec2d6318000db88811
Parth Buch <parth.buch.115@gmail.com>
2012-07-16 17:06:08 +0530

    Added on delete signal

---
+++ stapp/video/signals.py
@@ -0,0 +1,10 @@
+from django.core.files.storage import default_storage
+
+def delete_files(sender, **kwargs):
+    """Automatically deleted files when records removed.
+    """
+    video = kwargs.get('instance')
+    #default_storage.delete(school.pdf.path)
+    default_storage.delete(video.filename.path)
+    default_storage.delete(video.image.path)
+    default_storage.delete(video.thumbnail.path)
