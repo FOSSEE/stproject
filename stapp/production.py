6325dbee99165b956dd5a3e5034c0b627e318f5c
Parth Buch <parth.buch.115@gmail.com>
2012-07-19 23:07:49 +0530

    Removed database settings from production file, now required to provide a local.py file with db settings

--- stapp/production.py
+++ stapp/production.py
@@ -1,15 +1,4 @@
 from stapp.settings import *
 DEBUG=False
 TEMPLATE_DEBUG=DEBUG
-
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'stapp', # Or path to database file if using sqlite3.
-        'USER': 'stapp', # Not used with sqlite3.
-        'PASSWORD': 'pp@ssw0rd', # Not used with sqlite3.
-        'HOST': '', # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '', # Set to empty string for default. Not used with sqlite3.
-    }
-}
+from stapp.local import *
