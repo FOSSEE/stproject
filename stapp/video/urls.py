ef0ff535971b76e4420641f3aef8a472cd7c3dc5
Parth Buch <parth.buch.115@gmail.com>
2012-07-20 13:21:02 +0530

    Changes the urls to serve from root url

--- stapp/video/urls.py
+++ stapp/video/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns('video.views',
-    url(r'^view/$', 'show'),
+    url(r'^$', 'show'),
     url(r'^view/(?P<video_id>\d+)/$', 'show'),
 )
 
