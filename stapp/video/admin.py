1c9f444205a42394986b0cdb4def7ea289acc887
Parth Buch <parth.buch.115@gmail.com>
2012-07-20 18:20:49 +0530

    Added Dojo's dijit.Editor

--- stapp/video/admin.py
+++ stapp/video/admin.py
@@ -1,5 +1,28 @@
 from video.models import Video, Module
 from django.contrib import admin
+from django.contrib.admin import site, ModelAdmin
 
-admin.site.register(Video)
-admin.site.register(Module)
+# we define our resources to add to admin pages
+class CommonMedia:
+  js = (
+    'https://ajax.googleapis.com/ajax/libs/dojo/1.6.0/dojo/dojo.xd.js',
+    '/static/admin/js/editor.js',
+  )
+  css = {
+    'all': ('/static/admin/css/editor.css',),
+  }
+
+# let's add it to this model
+site.register(Video,
+  list_display = ('description',),
+  search_fields = ['description',],
+  Media = CommonMedia,
+)
+
+site.register(Module,
+  list_display = ('description',),
+  search_fields = ['description',],
+  Media = CommonMedia,
+)
+#admin.site.register(Video)
+#admin.site.register(Module)
